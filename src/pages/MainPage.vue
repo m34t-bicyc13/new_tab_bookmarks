e6ed2235315eb647c07bf7dc925f587522adf891
m34t_bicycl3 <meat.bicycl3@gmail.com>
2025-07-08 03:09:54 +0300

    Создан начальный компонент для закладок и добавлена типизация chrome в tsconfig

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,11 +1,13 @@
 ﻿<template>
   <div class="_main_container">
     <SearchComponent />
+    <BookmarksComponent />
   </div>
 </template>
 
 <script setup lang="ts">
 import SearchComponent from "../components/SearchComponent.vue";
+import BookmarksComponent from "../components/BookmarksComponent.vue";
 </script>
 
 <style scoped>
