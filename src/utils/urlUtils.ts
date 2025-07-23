1367367d5025c8a4162703a8dd186c1683ad117f
Makar Bondar <makar.bondar@rt.ru>
2025-07-23 11:12:29 +0300

    Для получения иконки по умолчанию для закладок теперь используется кеш favicon от Chrome.

--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -1,11 +1,11 @@
 ﻿// Получить URL иконки для закладки
 export const getFaviconUrl = (url: string): string => {
   try {
-    // Используем 'google.com' вместо 'www.google.com' для надежности
-    const hostname = new URL(url).hostname;
-    return `https://www.google.com/s2/favicons?domain=${hostname}&sz=32`;
+    const faviconUrl = new URL(chrome.runtime.getURL("/_favicon/"));
+    faviconUrl.searchParams.set("pageUrl", url);
+    faviconUrl.searchParams.set("size", "32");
+    return faviconUrl.toString();
   } catch {
-    // Возвращаем пустую строку для невалидных URL (например, 'javascript:...')
     return "";
   }
 };
